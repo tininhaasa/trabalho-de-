25c5932a78ad726af961e8570f150f228125e650
tininhaasa <tininhakook@gmail>
2021-06-16 22:24:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho de Introdução Web</title>
+
+    <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font.css">
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+</head>
+<body>
+    
+    <header>
+        <nav class="navbar navbar-dark">
+            <div class="container">
+                <img src="assets/img/logo.png" alt="">
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+        <div class="banner-home">
+            <div class="banner-img"></div>
+            <div class="background-filter"></div>
+
+            <h1 class="sanscd">
+                Sistema de Informação <br>
+                <small>Venha conhecer um dos melhores cursos da UNIPLAC</small>
+            </h1>
+        </div>
+
+    </main>
+
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
