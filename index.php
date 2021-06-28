860200b5044b09785043da018edf1439908c55b2
tininhaasa <tininhakook@gmail>
2021-06-28 19:54:49 -0300

    alterações

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <title>Trabalho de Introdução Web</title>
 
     <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
@@ -31,6 +30,52 @@
                 Sistema de Informação <br>
                 <small>Venha conhecer um dos melhores cursos da UNIPLAC</small>
             </h1>
+        </div>
+
+        <div class="teachers-banner row py-4">
+            <div class="col-12 pb-4">
+                <h2 class="text-center">Venha conhecer os professores do primeiro ano de curso e suas respectivas matérias</h2>
+            </div>
+            <div class="col  col-md-3">
+                <div class="card" style="width: 100%;">
+                    <img src="" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="col  col-md-3">
+                <div class="card" style="width: 100%;">
+                    <img src="" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="col  col-md-3">
+                <div class="card" style="width: 100%;">
+                    <img src="" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="col  col-md-3">
+                <div class="card" style="width: 100%;">
+                    <img src="" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        
         </div>
 
     </main>
