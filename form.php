fe5138bbe890955b9b4b65d74bc3ceb417964913
tininhaasa <tininhakook@gmail>
2021-06-28 21:44:19 -0300

    alterações

---
+++ form.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Trabalho de Introdução Web</title>
+
+    <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font.css">
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+</head>
+<body>
+    
+    <header>
+        <nav class="navbar navbar-dark">
+            <div class="container">
+                <img src="assets/img/logo.png" alt="">
+            </div>
+        </nav>
+    </header>
+<main>
+    <section class="formulario container text-center">
+            <h3>Questionário</h3>
+            <form method="GET">
+                <div class="question">
+                    <p>Você tem afinidade com matemática?</p>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q1" value="4" class="q1">
+                        <label class="form-check-label" for="inlineRadio1">Muito</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q1" value="3" class="q1">
+                        <label class="form-check-label" for="inlineRadio1">Um pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q1" value="2" class="q1">
+                        <label class="form-check-label" for="inlineRadio1">Talvez</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q1" value="1" class="q1">
+                        <label class="form-check-label" for="inlineRadio1">Muito pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q1" value="0" class="q1">
+                        <label class="form-check-label" for="inlineRadio1">Não</label>
+                    </div>
+                </div>
+                <div class="question">
+                    <p>Você tem interesse em empreender?</p>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q2" value="4" class="q2">
+                        <label class="form-check-label" for="inlineRadio1">Muito</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q2" value="3" class="q2">
+                        <label class="form-check-label" for="inlineRadio1">Um pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q2" value="2" class="q2">
+                        <label class="form-check-label" for="inlineRadio1">Talvez</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q2" value="1" class="q2">
+                        <label class="form-check-label" for="inlineRadio1">Muito pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q2" value="0" class="q2">
+                        <label class="form-check-label" for="inlineRadio1">Não</label>
+                    </div>
+                </div>
+                <div class="question">
+                    <p>Você gosta de informática?</p>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q3" value="4" class="q3">
+                        <label class="form-check-label" for="inlineRadio1">Muito</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q3" value="3" class="q3">
+                        <label class="form-check-label" for="inlineRadio1">Um pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q3" value="2" class="q3">
+                        <label class="form-check-label" for="inlineRadio1">Talvez</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q3" value="1" class="q3">
+                        <label class="form-check-label" for="inlineRadio1">Muito pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q3" value="0" class="q3">
+                        <label class="form-check-label" for="inlineRadio1">Não</label>
+                    </div>
+                </div>
+                <div class="question">
+                    <p>Você sabe trabalhar sob pressão?</p>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q4" value="4" class="q4">
+                        <label class="form-check-label" for="inlineRadio1">Muito</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q4" value="3" class="q4">
+                        <label class="form-check-label" for="inlineRadio1">Um pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q4" value="2" class="q4">
+                        <label class="form-check-label" for="inlineRadio1">Talvez</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q4" value="1" class="q4">
+                        <label class="form-check-label" for="inlineRadio1">Muito pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q4" value="0" class="q4">
+                        <label class="form-check-label" for="inlineRadio1">Não</label>
+                    </div>
+                </div>
+                <div class="question">
+                    <p>Você se considera criativo(a)?</p>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q5" value="4" class="q5">
+                        <label class="form-check-label" for="inlineRadio1">Muito</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q5" value="3" class="q5">
+                        <label class="form-check-label" for="inlineRadio1">Um pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q5" value="2" class="q5">
+                        <label class="form-check-label" for="inlineRadio1">Talvez</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q5" value="1" class="q5">
+                        <label class="form-check-label" for="inlineRadio1">Muito pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q5" value="0" class="q5">
+                        <label class="form-check-label" for="inlineRadio1">Não</label>
+                    </div>
+                </div>
+                <div class="question">
+                    <p>Você tem afinidade com a língua inglesa?</p>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q6" value="4" class="q6">
+                        <label class="form-check-label" for="inlineRadio1">Muito</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q6" value="3" class="q6">
+                        <label class="form-check-label" for="inlineRadio1">Um pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q6" value="2" class="q6">
+                        <label class="form-check-label" for="inlineRadio1">Talvez</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q6" value="1" class="q6">
+                        <label class="form-check-label" for="inlineRadio1">Muito pouco</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="q6" value="0" class="q6">
+                        <label class="form-check-label" for="inlineRadio1">Não</label>
+                    </div>
+                </div>
+
+                <input type="button" class="btn btn-success" value="Calcular Média" onclick="calculoMedia();">
+                <!-- <div id="resultado">Resultado:</div> -->
+            </form>
+    </section>
+
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/sweetalert/dist/sweetalert2.all.min.js"></script>
+    <script>
+        function calculoMedia(){
+
+            //transformar valores em variáveis
+            var p1 = document.querySelector('input[name="q1"]:checked').value
+            var p2 = document.querySelector('input[name="q2"]:checked').value
+            var p3 = document.querySelector('input[name="q3"]:checked').value
+            var p4 = document.querySelector('input[name="q4"]:checked').value
+            var p5 = document.querySelector('input[name="q5"]:checked').value
+            var p6 = document.querySelector('input[name="q6"]:checked').value
+            var res = document.getElementById('resultado')
+            
+            //transforma variáveis em números
+            var n1 = parseInt(p1)
+            var n2 = parseInt(p2)
+            var n3 = parseInt(p3)
+            var n4 = parseInt(p4)
+            var n5 = parseInt(p5)
+            var n6 = parseInt(p6)
+
+            //operação
+            var media = (n1 + n2 + n3 + n4 + n5 + n6) / 6;
+            // var media = n1 + n2 + n3 + n4 + n5 + n6
+            
+             if (media <= 1) {
+                swal({
+                    type: 'error',
+                    title: 'Teste de Personalidade',
+                    text: 'Sinto muito, acho que nosso curso não vai lhe agradar',
+                });
+                
+            }else if (media <= 2) {
+                swal({
+                    type: 'warning',
+                    title: 'Teste de Personalidade',
+                    text: 'Continue procurando, seu curso dos sonhos está por perto',
+                });
+                
+            }else if (media <= 3) {
+                swal({
+                    type: 'success',
+                    title: 'Teste de Personalidade',
+                    text: 'Você irá se encaixar em algum aspecto do curso',
+                });
+                
+            }else if (media <= 4) {
+                swal({
+                    type: 'success',
+                    title: 'Teste de Personalidade',
+                    text: 'Esse é o curso ideal pra você!',
+                });
+                
+            }
+            // res.innerHTML = (`A média é = <strong>${media}</strong>`)
+        }
+
+    </script>
+    </main>
+</body>
+</html>
