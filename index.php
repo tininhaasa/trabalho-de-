877fac40de5ff592cb99f5d924577328d15af4f5
Lidia Almeida <lidiamaria.almeida2000@gmail.com>
2021-06-29 22:09:37 -0300

    Trabalho Dev

--- index.php
+++ index.php
@@ -108,13 +108,12 @@
         
         </div>
 
-        <div class="row">
-            <div class="col-md-4">
-                <img src="">
-            </div>
+        <div class="row row-parag">
+            <div class="col-md-4 img1"></div>
 
-            <div class="col-md-8">
-                <p>O curso de Sistemas de Informação tem como objetivo ensinar o aluno a criar, adaptar e instalar programas que permitam o fácil acesso de dados, permitindo total 
+            <div class="col-md-8 py-5">
+                <h5 class="title title1">Objetivo do curso</h5>
+                <p class="parag">O curso de Sistemas de Informação tem como objetivo ensinar o aluno a criar, adaptar e instalar programas que permitam o fácil acesso de dados, permitindo total 
                     criatividade em seus conteúdos, criando soluções tecnológicas para determinar certo fluxo de informações. Além também de colocar o aluno mais facilmente no mercado
                      de trabalho, pois várias empresas fazem uso dos serviços do profissional de Sistemas. Porém, antes mesmo de se formar, o curso facilita ao aluno estagiar em 
                      diversos locais, permitindo uma rápida inserção no mercado de trabalho. Desenvolvimento de Software, Modelagem, Marketing e Vendas, Fluxo de Informações, 
@@ -122,12 +121,46 @@
             </div>
         </div>
 
+        <div class="row row-parag">
+            <div class="col-md-8  py-5">
+                <h5 class="title title2">Perfil do estudante</h5>
+                <p class="parag">Alguns requisitos são necessários, trabalhar bem sob pressão e prazos curtos é essencial para quem quer seguir essa carreira. Assim como ter afinidade 
+                    com inglês, matemática e tecnologia também é muito importante. Com o avanço da tecnologia, a busca por pessoas qualificadas nessa área aumentou cada vez mais, por 
+                    isso a importância da aproximação com tais conteúdos. A identificação de problemas e suas soluções é essencial para fazer parte desse meio. Disciplina e organização 
+                    também devem fazer parte do cotidiano desse profissional. Algumas disciplinas são mais específicas, têm-se Banco de Dados, Engenharia de Software, Arquitetura de 
+                    Computadores, entre outras. Porém, Matemática, Língua Portuguesa e Algoritmos, por exemplo, são a base para as disciplinas que ainda vão ser estudadas.</p>
+            </div>
+
+            <div class="col-md-4 img2"></div>
+        </div>
+
+        <div class="row row-parag">
+            <div class="col-md-4 img3"></div>
+
+            <div class="col-md-8 py-5">
+                <h5 class="title title3">Sistemas de Informação na UNIPLAC</h5>
+                <p class="parag">O primeiro ano do curso de Sistemas de Informação na UNIPLAC já introduz o aluno à base do conhecimento sobre tecnologia, como também possui 
+                    profissionais qualificados para ensinar e auxiliar os alunos nas aulas teóricas e laboratoriais. A preparação para o mercado de trabalho começa desde a primeira fase, 
+                    pois a busca pelo profissional desta área é abundante, já começando pelo estágio a partir do primeiro semestre, visto que não é preciso necessariamente ter muita 
+                    experiência. Além de ensinar o fundamental sobre tecnologia, o curso também instrui o estudante sobre gestão, liderança, e sobre como ele pode atuar como empreendedor, 
+                    analista, gerente e muito mais. São feitos muitos trabalhos individuais e em grupo, podendo citar o Projeto Integrador a cada fim de semestre, que visa unir os alunos 
+                    para colaborarem num projeto que junta todas as matérias daquele semestre.</p>
+            </div>
+        </div>
+
+
     </main>
+
+    <footer>
+        
+    </footer>
+
+
 
     <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrit1y="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
 </body>
 </html>
